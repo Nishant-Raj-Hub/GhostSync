f95d622e49994eb0d3a2ddce519735e5b94de130
Nishant Raj <nishantraj2109@gmail.com>
2024-07-08 14:54:34 +0530

    Fixed bugs

--- server/index.js
+++ server/index.js
@@ -34,11 +34,6 @@ app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 
-
-app.use('/', (req, res)=>{
-  res.send('Hello Noddy');
-})
-
 /* FILE STORAGE */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
